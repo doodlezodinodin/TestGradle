43d6f8ece16b24add1d8444e9db6b2c7d90a1aa9
doodlezodinodin <doodlezodinodin@gmail.com>
2017-05-16 11:52:34 +0300

    First commit. v2.0

---
+++ src/main/java/ua/company/teamDev/Main.java
@@ -0,0 +1,8 @@
+package ua.company.teamDev;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle");
+        System.out.println("One two");
+    }
+}
