949c2a06e3c4a34bc98336ae34cbb42745e1688f
doodlezodinodin <doodlezodinodin@gmail.com>
2017-05-16 11:59:19 +0300

    Empty file.

--- src/main/java/ua/company/teamDev/Main.java
+++ src/main/java/ua/company/teamDev/Main.java
@@ -2,7 +2,6 @@ package ua.company.teamDev;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Gradle");
-        System.out.println("One two");
+
     }
 }
